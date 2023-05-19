fa32ddd7807b26dc6847dc8dce4034252a002c30
rodric74 <rodric.rambosson@gmail.com>
2023-05-19 20:23:04 +0200

    partie login+affichage edition ok

--- FrontEnd/assets/editor.js
+++ FrontEnd/assets/editor.js
@@ -1,36 +1,43 @@
-//CHANGER login EN logout 
 window.onload = function() {
     const loginLink = document.getElementById('login--link');
   
     // Vérifie si un token d'authentification existe dans le stockage local
     const token = localStorage.getItem('token');
-  
+
+    const editBar = document.getElementById('edit-bar');  // <== move this line up here
+
     if (token) {
       // Si un token existe, change le texte du lien en "Logout"
       loginLink.textContent = 'Logout';
-    
-      // Ajoute un message de console pour confirmer qu'un utilisateur est connecté
-      console.log('Utilisateur connecté avec succès');
-
-      // Si un token existe, retire l'attribut hidden pour rendre tous les éléments visibles
+ 
+        // Si un token existe, retire l'attribut hidden pour rendre tous les éléments visibles
         const hiddenElements = document.querySelectorAll('div[hidden]');
         hiddenElements.forEach(element => {
-        element.removeAttribute('hidden');
+            element.removeAttribute('hidden');
         });
+
+        // Ajoute la classe .connected à #edit-bar pour le rendre visible
+        editBar.classList.add('connected');
+        
+        // Cache le conteneur de filtres
+        const filterContainer = document.querySelector('.categorieFilter');
+        filterContainer.style.display = 'none';
+
+        // Ajoute un gestionnaire d'événements de clic pour déconnecter l'utilisateur
+        loginLink.addEventListener('click', (event) => {
+            event.preventDefault();
   
-      // Ajoute un gestionnaire d'événements de clic pour déconnecter l'utilisateur
-      loginLink.addEventListener('click', (event) => {
-        event.preventDefault();
+            // Supprime le token du stockage local
+            localStorage.removeItem('token');
+
+            // Supprime la classe .connected de #edit-bar pour le cacher
+            editBar.classList.remove('connected');
   
-        // Supprime le token du stockage local
-        localStorage.removeItem('token');
+            // Change le texte du lien en "Login"
+            loginLink.textContent = 'Login';
   
-        // Change le texte du lien en "Login"
-        loginLink.textContent = 'Login';
-  
-        // Redirige vers la page d'accueil
-        window.location.href = 'index.html';
-      });
+            // Redirige vers la page d'accueil
+            window.location.href = 'index.html';
+        });
     }
 };
-  
