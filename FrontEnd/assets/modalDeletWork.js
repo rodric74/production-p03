2237adb5d3f85262cd676eeec573de31390643a7
rodric74 <rodric.rambosson@gmail.com>
2023-05-20 20:19:54 +0200

    ajout trash sur thumb en prévision supp

--- FrontEnd/assets/modalDeletWork.js
+++ FrontEnd/assets/modalDeletWork.js
@@ -1,5 +1,6 @@
-import {displayWorks} from "./index.js";
+import {fetchWorks} from "./data.js";
 
+// FERMETURE DE LA MODALE 
 document.addEventListener('DOMContentLoaded', () => {
     const modal = document.querySelector('.modal-delete-work-container');
     const closeModalButton = document.querySelector('.close-modal-btn');
@@ -8,4 +9,24 @@ document.addEventListener('DOMContentLoaded', () => {
     closeModalButton.addEventListener('click', () => modal.style.display = "none");
     overlay.addEventListener('click', () => modal.style.display = "none");
   });
-  
+
+// AFFICHER LES WORKS EN VIGNETTE
+document.addEventListener('DOMContentLoaded', async () => {
+  const modalFigure = document.querySelector('.modal-delete-figure');
+
+// Générer les vignettes des works dans la modal
+async function thumbnails() {
+  const works = await fetchWorks();
+  modalFigure.innerHTML = works.map(work => `
+    <figure class="work-item-modal">
+      <img src="${work.imageUrl}" alt="${work.title}" />
+      <img src="./assets/icons/trash.svg" alt="Supprimer l'oeuvre" id="delete-icon">
+    </figure>
+  `).join("");
+}
+
+
+  // Appeler la fonction pour générer les vignettes dans la modal
+  await thumbnails();
+});
+
