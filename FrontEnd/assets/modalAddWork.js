8561667f4a49072dd0abf414c5d1326d4b3c3adf
rodric74 <rodric.rambosson@gmail.com>
2023-05-21 00:35:42 +0200

    ajout des catégories dans modal-addwork-categorie

--- FrontEnd/assets/modalAddWork.js
+++ FrontEnd/assets/modalAddWork.js
@@ -1,12 +1,13 @@
 import{fetchCategories} from './data.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     const addWorkModal = document.querySelector('.modal-addWork-container');
     const deleteWorkModal = document.querySelector('.modal-delete-work-container');
     const closeButton = document.querySelector('.modal-addWork-container .close-modal-btn');
     const overlay = document.querySelector('.modal-addWork-container .overlay');
     const backButton = document.querySelector('.modal-addWork-container .arroback-btn');
-   
+    const categorieInput = document.querySelector('.modal-addwork-categorie');
+
     //FERMETURE MODALE
     const closeAddWorkModal = () => {
       addWorkModal.style.display = 'none';
@@ -19,5 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
   
     closeButton.addEventListener('click', closeAddWorkModal);
     overlay.addEventListener('click', closeAddWorkModal);
+
+    // Récupérer les catégories
+  const categories = await fetchCategories();
+
+  // Générer les options des catégories
+  const options = categories.map((category) => `
+    <option value="${category.id}">${category.name}</option>
+  `);
+
+  // Ajouter les options au champ de sélection
+  categorieInput.innerHTML = `
+    <option value=""</option>
+    ${options.join('')}
+  `;
   });
   
